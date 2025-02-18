4009a497a15316809ac93963604c84f32ca8429d
RUSHi-VAiRALE <rushivairale932@gmail.com>
2025-02-18 15:43:47 +0530

    small change in mobile view

--- src/components/about/StatsSection.js
+++ src/components/about/StatsSection.js
@@ -69,7 +69,7 @@ export default function StatsSection() {
       className="bg-[#e7edff] py-10 md:py-16"
     >
       <div className="container mx-auto px-4 md:px-8 lg:px-16 max-w-7xl">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
           {stats.map((stat, index) => (
             <div 
               key={index} 
