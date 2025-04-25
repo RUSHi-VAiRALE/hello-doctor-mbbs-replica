530ee93eb96670711d83cc2a138340b5f99d2bed
NikhiZencodx <nikhil.zencodx@gmail.com>
2025-04-25 23:44:37 +0530

    Updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['placehold.co','firebasestorage.googleapis.com','drive.google.com','m.media-amazon.com' ,'clatwallah.netlify.app', 'cdn.pixabay.com', 'www.istockphoto.com','s6.imgcdn.dev'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.googleapis.com',
-      },
-    ],
+    unoptimized: true
   },
 };
 
