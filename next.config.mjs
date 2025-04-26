89f4f95df53394ce720299ed10b880f4beb856db
NikhiZencodx <nikhil.zencodx@gmail.com>
2025-04-26 14:19:19 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['placehold.co','firebasestorage.googleapis.com','drive.google.com','m.media-amazon.com' ,'clatwallah.netlify.app', 'cdn.pixabay.com', 'www.istockphoto.com','s6.imgcdn.dev'],
-    unoptimized: true
+    unoptimized: false
   },
 };
 
